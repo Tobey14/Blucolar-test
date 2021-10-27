10e3154bfd10d0de9c11f2c30478b606610c1187
Tobey14 <sureboytobi@gmail.com>
2021-10-27 12:49:19 +0100

    deploy Blucolar test to github

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route path="/github">
         <App />
